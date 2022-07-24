c87eaf3216c1a3bee02299d8d9a58b9dcbe82303
karemored <karemored@gmail.com>
2022-07-24 14:41:17 -0700

    solution to lab-1

--- conf/env.mk
+++ conf/env.mk
@@ -17,4 +17,5 @@ V = @
 # If the makefile cannot find your QEMU binary, uncomment the
 # following line and set it to the full path to QEMU.
 #
-# QEMU=
+#QEMU=home/divyansh/JOS_Kernel/qemu/bin/qemu-system-i386
+QEMU=/home/divyansh/kernel/qemu/bin/qemu-system-i386
